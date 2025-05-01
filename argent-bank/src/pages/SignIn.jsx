39e5933b6a34bc7b2d7595a8add91a6f4cf36d3c
maroua-MEDDEB <70540750+maroua-MEDDEB@users.noreply.github.com>
2025-05-01 12:39:02 +0200

    modification backend pour creation users automatique + concurrently pour lancer backend et frontend simultanement + consommation api endpoint login

--- argent-bank/src/pages/SignIn.jsx
+++ argent-bank/src/pages/SignIn.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-
+import axios from "axios";
 function SignIn() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  function handleLogin(e) {
+    // bloquer l'action par default de balise <form> </form> cest le refresh !
+    e.preventDefault();
+    axios
+      .post("http://localhost:3001/api/v1/user/login", data)
+      .then((result) => {
+        console.log(result.data);
+        localStorage.setItem("token", result.data.body.token);
+      });
+  }
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
-        <form>
+        <form onSubmit={handleLogin}>
           <div className="input-wrapper">
-            <label for="username">Username</label>
-            <input type="text" id="username" />
+            <label for="username">email</label>
+            <input
+              type="text"
+              id="username"
+              onChange={(e) => setData({ ...data, email: e.target.value })}
+            />
           </div>
           <div className="input-wrapper">
             <label for="password">Password</label>
-            <input type="password" id="password" />
+            <input
+              type="password"
+              id="password"
+              onChange={(e) => setData({ ...data, password: e.target.value })}
+            />
           </div>
           <div className="input-remember">
             <input type="checkbox" id="remember-me" />
             <label for="remember-me">Remember me</label>
           </div>
 
-          <NavLink to="/userInfos" className="sign-in-button">
+          <button type="submit" className="sign-in-button">
             Sign In
-          </NavLink>
+          </button>
         </form>
       </section>
     </main>
--- backend/server/scripts/populateDatabase.js
+++ backend/server/scripts/populateDatabase.js
@@ -1,24 +1,35 @@
-const axios = require('axios')
-const signupApi = 'http://localhost:3001/api/v1/user/signup'
+const axios = require("axios");
+const userModel = require("../database/models/userModel");
+const signupApi = "http://localhost:3001/api/v1/user/signup";
 
-const users = [
-  {
-    firstName: 'Tony',
-    lastName: 'Stark',
-    email: 'tony@stark.com',
-    password: 'password123'
-  },
-  {
-    firstName: 'Steve',
-    lastName: 'Rogers',
-    email: 'steve@rogers.com',
-    password: 'password456'
+async function populateDB() {
+  const users = [
+    {
+      firstName: "Tony",
+      lastName: "Stark",
+      email: "tony@stark.com",
+      password: "password123",
+    },
+    {
+      firstName: "Steve",
+      lastName: "Rogers",
+      email: "steve@rogers.com",
+      password: "password456",
+    },
+  ];
+  // recherche des utlisateurs existant
+  let existingUsers = await userModel.find();
+  // no doit ajouter les utilisateurs dans la base de donnes si et seulement si il ne sont pas creer donc collection vide
+  if (existingUsers.length != 0) {
+    console.log("users already exists");
+  } else {
+    users.forEach((user) => {
+      axios
+        .post(signupApi, user)
+        .then((response) => {})
+        .catch((error) => console.log(error));
+    });
   }
-]
+}
 
-users.forEach(user => {
-  axios
-    .post(signupApi, user)
-    .then(response => console.log(response))
-    .catch(error => console.log(error))
-})
+module.exports = populateDB;
